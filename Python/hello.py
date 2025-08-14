2f1cf3967261b611bc37f278e1b849019dbc20bb
saad-abdullah11e1 <181092@cloud.rangitoto.school.nz>
2025-08-15 10:54:29 +1200

    dddd

--- Python/hello.py
+++ Python/hello.py
@@ -1,8 +1,13 @@
 import tkinter as tk
+import time
 
 window = tk.Tk()
 
-greeting = tk.Label(window, text="Hello, World!")
+count = 0
+
+old_time = time.time()
+
+greeting = tk.Label(window, text=count)
 greeting.pack()
 
 button = tk.Button(
@@ -12,8 +17,32 @@ button = tk.Button(
     bg="blue",
     fg="yellow",
 )
-button.bind("<Button-1>", lambda event: greeting.config(text="Button Clicked!"))
-button.bind("<ButtonRelease>", lambda event: greeting.config(text="Hello, World!"))
+
+def on_click(event):
+    global count
+    global old_time
+    count += 1
+
+def update_clicks():
+    global count
+    global old_time
+    greeting.config(text=int(count/(time.time()-old_time)))
+    print(count)
+    button.after(100, update_clicks)
+
+def reset_timer():
+    global old_time
+    old_time = time.time()
+    global count
+    count = 0
+    greeting.config(text=int(count/(time.time()-old_time)))
+    print(count)
+    button.after(1000, reset_timer)
+
+button.bind("<Button-1>", on_click)
+button.after(1000, reset_timer)
+button.after(100, update_clicks)
+
 
 button.pack()
 
