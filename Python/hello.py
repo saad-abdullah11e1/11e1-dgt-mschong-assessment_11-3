b5a00eac4f34be0b2d02294b6ed80ec2073c5449
saad-abdullah11e1 <181092@cloud.rangitoto.school.nz>
2025-08-15 10:30:36 +1200

    init

--- Python/hello.py
+++ Python/hello.py
@@ -1,1 +1,20 @@
-print("Hello world!")
+import tkinter as tk
+
+window = tk.Tk()
+
+greeting = tk.Label(window, text="Hello, World!")
+greeting.pack()
+
+button = tk.Button(
+    text="Click me!",
+    width=25,
+    height=5,
+    bg="blue",
+    fg="yellow",
+)
+button.bind("<Button-1>", lambda event: greeting.config(text="Button Clicked!"))
+button.bind("<ButtonRelease>", lambda event: greeting.config(text="Hello, World!"))
+
+button.pack()
+
+window.mainloop()
